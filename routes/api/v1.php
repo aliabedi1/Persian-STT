9eb25bd1b02c51534a7470a7d37a605a6495d2ee
Ali Abedi <a.abedi@dornica.net>
2024-07-27 23:42:15 +0330

    add profile section routes

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,17 +1,19 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Authentication\AuthenticationController;
+use App\Http\Controllers\Api\V1\Profile\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('authentication')->as('authentication')->group(function () {
+Route::prefix('authentication')->as('authentication.')->group(function () {
     Route::post('login', [AuthenticationController::class, 'login'])->name('login');
     Route::post('verify', [AuthenticationController::class, 'verify'])->name('verify');
     Route::post('logout', [AuthenticationController::class, 'logout'])->middleware('auth:api-user')->name('logout');
-
 });
 
 Route::middleware('auth:api-user')->group(function () {
-
-
+    Route::prefix('profile')->as('profile.')->group(function () {
+        Route::get('current', [ProfileController::class, 'current'])->name('current');
+        Route::post('update', [ProfileController::class, 'update'])->name('update');
+    });
 });
 
