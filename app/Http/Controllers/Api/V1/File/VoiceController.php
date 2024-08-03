b1071128225662aad9f085d22a033b9eb8c95d7d
Ali Abedi <a.abedi@dornica.net>
2024-08-04 02:22:18 +0330

    finished functions for upload and delete voices

--- app/Http/Controllers/Api/V1/File/VoiceController.php
+++ app/Http/Controllers/Api/V1/File/VoiceController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Api\V1\File;
 
+use App\Enums\SystemMessage;
+use App\Enums\VoiceStatus;
+use App\Exceptions\Api\ApiException;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\V1\File\UploadFileRequest;
-use App\Http\Resources\File\FileResource;
+use App\Http\Requests\Api\V1\Voice\DeleteVoiceRequest;
+use App\Http\Requests\Api\V1\Voice\UploadVoiceRequest;
+use App\Http\Resources\Voice\VoiceResource;
 use App\Models\Voice;
 use App\Services\FileService;
-use App\Services\IOTypeService;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Storage;
 
 
 class VoiceController extends Controller
@@ -18,31 +20,88 @@ class VoiceController extends Controller
     /**
      * @throws \App\Exceptions\File\EntryFieldsMissMatchTableFillablesException
      * @throws \App\Exceptions\File\FileException
+     * @throws \Exception
      */
-    public function upload(UploadFileRequest $request)
+    public function upload(UploadVoiceRequest $request)
     {
-        $uploader = new FileService();
-        $disk = $uploader->getTypeFromConfig()['disk'];
-        $uploadedFile = $uploader->upload();
-        $apiResponse = (new IOTypeService(Storage::disk($disk)->get($uploadedFile['name'])))->sendRequest();
-        dd($apiResponse->body());
-        if ($apiResponse->successful()){
-            dd($result);
-            $result = $apiResponse->json()['result'];
+        $fileService = new FileService();
+        $uploadedFile = $fileService->upload();
 
 
-
-        }   else{
-            dd($apiResponse->json());
+        try {
+            $text = getTextFromSpeech($uploadedFile['url']);
+        } catch (ApiException $exception) {
+            $fileService->deleteFile($uploadedFile['id']);
+            return Response::error(
+                code: SystemMessage::API_ERROR,
+                message: __('Calling api endpoints faced some issues, please try again later.'),
+            );
         }
 
+        $voice = Voice::create([
+            'user_id' => auth()->id(),
+            'voice_file_id' => $uploadedFile['id'],
+            'text' => $text,
+            'status' => VoiceStatus::SUCCESS,
+        ]);
 
-
-        $uploader->getTypeFromConfig();
 
         return Response::success(
             message: __('File uploaded successfully.'),
-            data: new FileResource($uploadedFile)
+            data: new VoiceResource($voice)
+        );
+    }
+
+
+    /**
+     * @throws \App\Exceptions\File\FileException
+     */
+    public function delete(DeleteVoiceRequest $request)
+    {
+        $voiceIDs = $request->input('files');
+        $fileService = new FileService();
+
+        $multiple = false;
+        if (sizeof($voiceIDs) > 1) {
+            $multiple = true;
+        }
+
+        if (empty($voiceIDs)) {
+            $multiple = true;
+
+            auth('api-user')
+                ->user()
+                ->voices()
+                ->delete();
+
+            $fileService
+                ->deleteFile(
+                    auth('api-user')
+                        ->user()
+                        ->voice_files
+                        ->pluck('id')
+                        ->toArray()
+                );
+        } else {
+            $voiceFileIDs = auth('api-user')
+                ->user()
+                ->voices()
+                ->whereIn('id', $voiceIDs)
+                ->get()
+                ->pluck('id')
+                ->toArray();
+
+            auth('api-user')
+                ->user()
+                ->voices()
+                ->whereIn('id', $voiceIDs)
+                ->delete();
+
+            $fileService->deleteFile($voiceFileIDs);
+        }
+
+        return Response::destroy(
+            message: $multiple ? __('Voices deleted successfully.') : __('Voice deleted successfully.'),
         );
     }
 
