7aec1cf5a5d4fe63c4040d1d6e0e0539070f7bf2
Ali Abedi <a.abedi@dornica.net>
2024-08-04 02:24:49 +0330

    change minor stuff in usages of functions and made it easier to use

--- app/Traits/FileUrlGenerator.php
+++ app/Traits/FileUrlGenerator.php
@@ -10,36 +10,32 @@ trait FileUrlGenerator
     /**
      * Get file url
      *
-     * @param string $type
      * @param string $field_name
      * @return string|null
      * @throws FileException
      */
-    public function getFileUrl(string $type, string $field_name): null|string
+    public function getFileUrl(string $field_name): null|string
     {
         if (!isset($this->$field_name) || $this->$field_name == '') return null;
 
         $service = new FileService();
         return $service
-            ->setType($type)
             ->getUrl($this->$field_name);
     }
 
     /**
      * Get file details
      *
-     * @param string $type
      * @param string $field_name
      * @return array|null
      * @throws FileException
      */
-    public function getFileDetails(string $type, string $field_name): null|array
+    public function getFileDetails(string $field_name): null|array
     {
         if (!isset($this->$field_name) || $this->$field_name == '') return null;
 
         $service = new FileService();
         return $service
-            ->setType($type)
             ->getDetails($this->$field_name);
     }
 }
