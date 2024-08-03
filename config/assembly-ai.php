a06b8e4aa54101da2d95484ea1859c8650ed6109
Ali Abedi <a.abedi@dornica.net>
2024-08-04 02:22:51 +0330

    change config file based on changing api

---
+++ config/assembly-ai.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'API_URL' => 'https://api.assemblyai.com/v2/transcript',
+    'LANGUAGE_CODE' => 'fa',
+    'SPEECH_MODEL' => 'nano',
+    'TOKEN' => '75252b090dbc495b83cbe73794f53e94',
+
+];
--- config/iotype.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'API_URL' => 'https://www.iotype.com/developer/transcription',
-    'DEFAULT_TYPE' => 'file',
-    'TOKEN' => '0ixJe7U7aTkcqyjHnKsX3OP8AUerM8JL',
-
-];
