fe1c1012641056b9265fcf09a4cd647df945edef
Ali Abedi <a.abedi@dornica.net>
2024-08-10 01:26:44 +0330

    add about me endpoint

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Authentication\AuthenticationController;
+use App\Http\Controllers\Api\V1\General\InformationController;
 use App\Http\Controllers\Api\V1\Voice\VoiceController;
 use App\Http\Controllers\Api\V1\Profile\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -14,12 +15,15 @@ Route::prefix('authentication')->as('authentication.')->group(function () {
 Route::middleware('auth:api-user')->group(function () {
     Route::prefix('profile')->as('profile.')->group(function () {
         Route::get('current', [ProfileController::class, 'current'])->name('current');
-        Route::post('update', [ProfileController::class, 'update'])->name('update');
+        Route::put('update', [ProfileController::class, 'update'])->name('update');
     });
     Route::prefix('voices')->as('voices.')->group(function () {
         Route::post('upload', [VoiceController::class, 'upload'])->name('upload');
         Route::post('delete', [VoiceController::class, 'delete'])->name('delete');
         Route::get('history', [VoiceController::class, 'history'])->name('history');
     });
+
+    Route::get('about-me', [InformationController::class, 'aboutMe'])->name('about-me');
+
 });
 
